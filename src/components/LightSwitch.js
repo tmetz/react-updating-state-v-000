ebf36346656c07a789d07d45f8002ffaf359c7c3
Tammy Metz <tammy@tammymetz.com>
2019-11-02 23:34:44 +0000

    Done.

---
+++ src/components/LightSwitch.js
@@ -0,0 +1,33 @@
+// This was an example given in code-a-long, but right now
+// it's not connected to index.js
+import React from 'react';
+
+class LightSwitch extends React.Component {
+  constructor() {
+    super();
+
+    // Initial state is defined
+    this.state = {
+      toggled: false
+    };
+  }
+
+  // when handleClick is called, setState will update the state so that toggle is reversed
+  handleClick = () => {
+    this.setState(previousState => {
+      return {
+        toggled: !previousState.toggled
+      }
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClick}>{this.state.toggled ? "ON" : "OFF"}</button>
+      </div>
+    );
+  }
+}
+
+export default LightSwitch;
